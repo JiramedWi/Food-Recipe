d5d39d89926063acc72ab01489e8f5e0bb3d0039
Jiramed <jiramed_wi@cmu.ac.th>
2022-03-13 00:15:31 +0700

    do search function by ranking with bm25

---
+++ BackEnd/src/fucntion/fuction.py
@@ -0,0 +1,38 @@
+import pickle
+
+import pandas as pd
+
+from bm25 import BM25
+from pre_process import pre_process
+
+food_df = pickle.load(open('../../resource/food_cleaned.pkl', 'rb'))
+title = pickle.load(open('../../resource/bm25_title.pkl', 'rb'))
+ingre = pickle.load(open('../../resource/bm25_ingre.pkl', 'rb'))
+
+
+def title_ranking(query):
+    score = title.transform(query)
+    tf = pd.DataFrame({'bm25': list(score),
+                       'Title': list(food_df['Title']),
+                       'Ingredient': list(food_df['Cleaned_Ingredients']),
+                       'Instructions': list(food_df['Instructions']),
+                       'Image': list(food_df['Image_Name'].apply(lambda s: s + '.jpg'))
+                       }).nlargest(columns='bm25', n=10)
+    tf['rank'] = tf['bm25'].rank(ascending=False)
+    tf = tf.drop(columns='bm25', axis=1)
+    tf = tf.to_dict('record')
+    return tf
+
+
+def ingredient_ranking(query):
+    score_ingre = ingre.transform(query)
+    tf = pd.DataFrame({'bm25': list(score_ingre),
+                       'Title': list(food_df['Title']),
+                       'Ingredient': list(food_df['Cleaned_Ingredients']),
+                       'Instructions': list(food_df['Instructions']),
+                       'Image': list(food_df['Image_Name'].apply(lambda s: s + '.jpg'))
+                       }).nlargest(columns='bm25', n=10)
+    tf['rank'] = tf['bm25'].rank(ascending=False)
+    tf = tf.drop(columns='bm25', axis=1)
+    tf = tf.to_dict('record')
+    return tf
